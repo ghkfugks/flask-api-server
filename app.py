c6d8b2d62ac352428efe50f994cab2f4a3e31a37
jung hyun <hfghhghg11@gmil.com>
2022-06-17 10:41:18 +0900

    app

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from crypt import methods
 from flask import Flask
 
 app = Flask(__name__)
 
-# API 가 있어야 한다 !! 아래 코드가 API
-@app.route('/',methods= ['GET'])
+# 실행하려면 API 가 있어야 한다! 아래 코드가 API
+
+# client가 '/'에서 GET 메쏘드를 주면 'Hello World' 를 리턴한다.
+@app.route('/', methods = ['GET'])   # '/' root 경로
 def hello_world():
     return 'Hello World'
 
 
 
+
 if __name__ == '__main__' :
     app.run()
-
-
-
